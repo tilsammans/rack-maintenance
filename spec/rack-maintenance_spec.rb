9009e7c9573a7ee469ecd4bd2e4fcfd7330393f2
Joost Baaij <joost@spacebabies.nl>
2012-09-28 22:19:19 +0200

    add missing spec cases

--- spec/rack-maintenance_spec.rb
+++ spec/rack-maintenance_spec.rb
@@ -5,6 +5,14 @@ describe "RackMaintenance" do
   let(:app) { Class.new { def call(env); end }.new }
   let(:rack) { Rack::Maintenance.new(app, :file => "spec/maintenance.html") }
 
+  context "without a :file option" do
+    it "raises an error" do
+      expect {
+        Rack::Maintenance.new(app)
+      }.to raise_error(ArgumentError)
+    end
+  end
+
   context "without maintenance file" do
     it "calls the app" do
       app.should_receive(:call).once
@@ -28,6 +36,28 @@ describe "RackMaintenance" do
 
     it "returns the maintenance response" do
       rack.call({}).should eq [503, {"Content-Type"=>"text/html", "Content-Length"=>"0"}, [""]]
+    end
+
+    context "and :env option MAINTENANCE" do
+      let(:rack) { Rack::Maintenance.new(app, :file => "spec/maintenance.html", :env => "MAINTENANCE") }
+
+      context "outside MAINTENANCE env" do
+        it "calls the app" do
+          app.should_receive(:call).once
+          rack.call({})
+        end
+      end
+
+      context "inside MAINTENANCE env" do
+        before do
+          ENV['MAINTENANCE'] = "true"
+        end
+
+        it "does not call the app" do
+          app.should_not_receive :call
+          rack.call({})
+        end
+      end
     end
 
     context "and request to /assets" do
